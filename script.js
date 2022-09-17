5c5af785fc6ac908ccf9d530e6b78a790bdd8b69
bharat chitara <bharatchitara99@gmail.com>
2022-09-18 00:02:30 +0530

    single_page_portfolio_commit1

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("pf-header-sidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("pf-header-sidenav").style.width = "0%";
+  }
+  
