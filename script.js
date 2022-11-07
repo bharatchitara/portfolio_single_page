fee2121ea5f3ac39f3794d074fa7052ebc04d712
bharat chitara <bharatchitara99@gmail.com>
2022-11-07 17:33:33 +0530

    final header completed with all media queries

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function openNav() {
     document.getElementById("pf-header-sidenav").style.width = "100%";
   }
   
-  function closeNav() {
-    document.getElementById("pf-header-sidenav").style.width = "0%";
-  }
+function closeNav() {
+  document.getElementById("pf-header-sidenav").style.width = "0%";
+}
   
